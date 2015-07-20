713a1188680d40a1736e36638ecc0168fbca292f
shamseen <shamseen.c@gmail.com>
2015-07-20 16:21:03 -0400

    Revert "making Movie class public and adding a context.."
    This reverts commit f986eacb2ff0e1e658cb798762599ec7faf59a98.

--- Lab6_ASPNET/Models/Movie.cs
+++ Lab6_ASPNET/Models/Movie.cs
@@ -6,17 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lab6_ASPNET.Models
 {
-    public class Movie
+    class Movie
     {
-        public int ID { get; set; }
-        public string Title { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public string Genre { get; set; }
-        public decimal Price { get; set; }
-    }
-
-    public class MovieDBContext : DbContext //object to hold all entities to populate db
-    {
-        public DbSet<Movie> Movies { get; set; } 
     }
 }
