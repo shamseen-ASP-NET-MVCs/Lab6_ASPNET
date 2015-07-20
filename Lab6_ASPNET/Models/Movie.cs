cdea0b2248a20167e6ac910d254360b94e35f4e5
shamseen <shamseen.c@gmail.com>
2015-07-20 16:29:55 -0400

    made a new Movie class in Models because
    System.Data.Entity wasn't entering.

--- Lab6_ASPNET/Models/Movie.cs
+++ Lab6_ASPNET/Models/Movie.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Lab6_ASPNET.Models
 {
-    class Movie
+    public class Movie
     {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
     }
 }
