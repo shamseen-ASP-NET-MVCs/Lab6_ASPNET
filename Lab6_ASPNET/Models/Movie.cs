fc4a9af9bdfc5cae6b890810973968092af8b12b
shamseen <shamseen.c@gmail.com>
2015-07-30 19:51:09 -0400

    adding type validation

--- Lab6_ASPNET/Models/Movie.cs
+++ Lab6_ASPNET/Models/Movie.cs
@@ -4,16 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Lab6_ASPNET.Models
 {
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 200)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+        [StringLength(30)]
         public string Rating { get; set; }
     }
 
